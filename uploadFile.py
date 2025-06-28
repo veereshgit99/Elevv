9dd53004a9ed3fc968f906be34c5911e39bad42a
Veeresh Koliwad <157433566+veereshgit99@users.noreply.github.com>
2025-06-27 19:03:03 -0700

    Further Changes - SQS queue

--- uploadFile.py
+++ uploadFile.py
@@ -1,7 +1,7 @@
 import requests
 
 upload_url = input()  # replace with your URL
-file_path = "./asu_sunburst_rgb_maroongold_150ppi_small.png"  # replace with your file
+file_path = "./Veeresh_Koliwad_Resume.pdf"
 
 with open(file_path, "rb") as f:
     response = requests.put(upload_url, data=f)
