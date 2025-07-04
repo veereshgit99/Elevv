50169301391c60cfa0f08ce2c8405a183e584e77
Veeresh Koliwad <157433566+veereshgit99@users.noreply.github.com>
2025-07-03 17:38:34 -0700

    File Upload

--- FileService/uploadFile.py
+++ FileService/uploadFile.py
@@ -1,7 +1,7 @@
 import requests
 
 upload_url = input()  # replace with your URL
-file_path = "./Veeresh_Koliwad_Resume.pdf"
+file_path = "./QuikPAY(R).pdf"
 
 with open(file_path, "rb") as f:
     response = requests.put(upload_url, data=f)
